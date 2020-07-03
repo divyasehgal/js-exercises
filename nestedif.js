d6d331c8d5f7f143ea869c1aa90bc0479b268f66
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-07-03 13:26:51 -0400

    nestedif

--- nestedif.js
+++ nestedif.js
@@ -1,8 +1,12 @@
-let a = 15;
+let a = 25;
 if (a % 2 === 0) {
   console.log(`Number ${a} is even`);
 } else if (a % 3 === 0 || a % 5 === 0) {
-  console.log(`Number ${a} is divisible by 3 or 5`);
+  if (a % 3 === 0) {
+    console.log(`Number ${a} is divisible by 3`);
+  } else {
+    console.log(`Number ${a} is divisible by 5`);
+  }
 } else {
   console.log(`Number ${a} is odd`);
 }
