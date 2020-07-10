2bcffc4bcf203d901274f66a915258b1741f48ae
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-07-09 23:21:49 -0400

    map functions

---
+++ map.js
@@ -0,0 +1,42 @@
+var a = [4, 6, 2, 2, 66, 33, 56, 33, 78, 112, 32, 20, 31, 12];
+
+var a = [2, 4, 6, 8];
+a.map((data) => {
+  console.log(data);
+});
+
+var a = [2, 4, 6, 8];
+a.map((data) => {
+  console.log(data * 2);
+});
+
+var a = [2, 4, 6, 8];
+a.map((data) => {
+  return data * 2;
+});
+
+var a = [4, 6, 2, 2, 66, 33, 56, 33, 78, 112, 32, 20, 31, 12];
+a.map((data) => {
+  return data > 20;
+});
+
+/*var a = [4, 6, 2, 2, 66, 33, 56, 33, 78, 112, 32, 20, 31, 12];
+a.map((data) => {
+  return data > 20;
+})(14)[
+  (false,
+  false,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false)
+];
+*/
