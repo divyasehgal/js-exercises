2bea02f28d55566446efd1b967c4b55daea6e6aa
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-07-15 19:07:00 -0400

    map examples

---
+++ map1.js
@@ -0,0 +1,46 @@
+const people = [
+  {
+    first_name: "CJ",
+    last_name: "R. ",
+  },
+  {
+    first_name: "Brendan",
+    last_name: "Eich",
+  },
+  {
+    first_name: "Kyle",
+    last_name: "Simpson",
+  },
+  {
+    first_name: "Douglas",
+    last_name: "Crockford",
+  },
+];
+
+const fullNames = people.map((person) => {
+  return `${person.first_name} ${person.last_name}`;
+});
+console.log(fullNames);
+
+const animals = [
+  {
+    name: "cat",
+    size: "small",
+  },
+  {
+    name: "dog",
+    size: "small",
+  },
+  {
+    name: "lion",
+    size: "medium",
+  },
+  {
+    name: "elephant",
+    size: "big",
+  },
+];
+
+const names = animals.map((animalName) => animalName.name);
+
+console.log(names);
