afcc279761c1a97c14218892c1da1d1384d03fe6
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-08-17 22:27:35 -0400

    object literals

---
+++ objectliteral.js
@@ -0,0 +1,10 @@
+var car = { manyCars: { a: "Saab", b: "Jeep" }, 7: "Mazda" };
+
+console.log(car.manyCars.b); // Jeep
+console.log(car[7]); // Mazda
+
+// var car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
+// console.log(car.myCar); // Saturn
+// console.log(car.getCar); // Honda
+// console.log(car.special); // Toyota
