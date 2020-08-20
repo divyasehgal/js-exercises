de4332d7167d9498539154d5d8cc1160a12e66bd
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-08-19 23:27:18 -0400

    shopping list

---
+++ shoppinglist.js
@@ -0,0 +1,8 @@
+let shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+console.log(shopping);
+
+let random = ["tree", 795, [0, 1, 2]];
+for (let i = 0; i < random.length; i++) {
+  console.log(random[i]);
+}
+//console.log(random[2]);
