7012b56efe3f757ab6408eacc182f11a02a471f6
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-08-20 23:47:29 -0400

    nested function

---
+++ nestedfunc.js
@@ -0,0 +1,9 @@
+function addSquares(a, b) {
+  function square(x) {
+    return x * x;
+  }
+  return square(a) + square(b);
+}
+a = addSquares(2, 3); // returns 13
+b = addSquares(3, 4); // returns 25
+c = addSquares(4, 5); // returns 41
