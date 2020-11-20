13a944b9aa6dfb4b541cac6613b83210f36d05d0
Divya Sehgal <divya.sehgal@Divya-Sehgals-MacBook-Pro.local>
2020-11-20 14:30:54 -0500

    call and apply

--- fundefault.js
+++ fundefault.js
@@ -50,3 +50,40 @@ greetHi('Divya');
 
 //Arror function for greet function
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+//Call and Apply methods 
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+lufthansa.book(239, 'Divya');
+lufthansa.book(635, 'Divya R.');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+book.call(eurowings, 434, 'Rahul');
+console.log(eurowings);
+book.call(lufthansa, 677, 'Sehgal');
+console.log(lufthansa);
+
+//Apply method 
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+book.call(swiss, ...flightData);
+
